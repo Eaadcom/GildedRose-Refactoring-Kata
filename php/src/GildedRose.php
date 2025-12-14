37b74732c22a061ef2657782ff2b98abdb1035ff
edwarddeen <eaadcom@gmail.com>
2025-12-14 17:07:48 +0100

    started refactoring the updateQuality function

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -18,26 +18,26 @@ final class GildedRose
     public function updateQuality(): void
     {
         foreach ($this->items as $item) {
-            if ($item instanceof AgedBrieItem) {
-                $item->update();
-                continue;
-            }
             if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
                 if ($item->quality > 0) {
                     if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+                        // Reduce Item quality by 1 of Regular Items
                         $item->quality = $item->quality - 1;
                     }
                 }
             } else {
                 if ($item->quality < 50) {
+                    // Add Item quality to Aged Brie & Backstage passes
                     $item->quality = $item->quality + 1;
                     if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
                         if ($item->sellIn < 11) {
+                            // Backstage passes `Quality` increases by `2` when there are `10` days or less
                             if ($item->quality < 50) {
                                 $item->quality = $item->quality + 1;
                             }
                         }
                         if ($item->sellIn < 6) {
+                            // Backstage passes `Quality` increases by `3` when there are `5` days or less
                             if ($item->quality < 50) {
                                 $item->quality = $item->quality + 1;
                             }
@@ -46,10 +46,12 @@ final class GildedRose
                 }
             }
 
+            // Reduces sellIn for all items except for Sulfuras
             if ($item->name != 'Sulfuras, Hand of Ragnaros') {
                 $item->sellIn = $item->sellIn - 1;
             }
 
+            // Reduces quality of an item by an additional point when sellIn is negative
             if ($item->sellIn < 0) {
                 if ($item->name != 'Aged Brie') {
                     if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
@@ -59,9 +61,11 @@ final class GildedRose
                             }
                         }
                     } else {
+                        // Sets item quality to 0
                         $item->quality = $item->quality - $item->quality;
                     }
                 } else {
+                    // Increases Aged brie by an extra point when sellIn is negative
                     if ($item->quality < 50) {
                         $item->quality = $item->quality + 1;
                     }
